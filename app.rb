58349475c3dfd31d5e24e7660bcb3e217b946eef
Alonzo Rice <96135977+ricealonzo@users.noreply.github.com>
2024-05-17 06:13:30 +0000

    j

--- app.rb
+++ app.rb
@@ -2,6 +2,19 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
+erb(:homepage)
+end
+get("/square/results") do
+  @num = params.fetch("num").to_f
+  @square = @num * @num
+  erb(:square_results)
+end
+get("/square/new") do
+ 
+  erb(:homepage)
+  
+end
+get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
