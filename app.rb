e19d9aedc77cbf27ca5abd2bd10b4d4673c9069f
Alonzo Rice <96135977+ricealonzo@users.noreply.github.com>
2024-05-20 21:31:35 +0000

    l

--- app.rb
+++ app.rb
@@ -4,16 +4,19 @@ require "sinatra/reloader"
 get("/") do
 erb(:homepage)
 end
+
 get("/square/results") do
   @num = params.fetch("num").to_f
   @square = @num * @num
   erb(:square_results)
 end
+
 get("/square/new") do
   
   erb(:homepage)
   
 end
+
 get("/square_root/new") do
   erb(:square_root)
 end
@@ -31,30 +34,38 @@ end
 
 get("/payment/results") do
   
+  
   #The is the variable that contains the 
   #calculations for "r" in th payment equation!!input apr using erb tags!!.
-  @wintrest_rate_per_period = (params.fetch("apr").to_f/100)/12
+  @apr = params.fetch("apr").to_i.to_f
+  @intrest_rate_per_period =(params.fetch("apr").to_i.to_f/100)/12
+  
   
   #this is the calculation for n(number of periods) in the equation
-  @number_of_monthly_periods = params.fetch("n") * 12
+  @number_of_years = params.fetch("number_of_years").to_i.to_f
+  @number_of_monthly_periods = @number_of_years * 12
   
   #this is the calculations for the numerator of the equation
-  @numerator = @intrest_rate_per_period * @present_value
+  @principal = params.fetch("principal")
+  @numerator = @intrest_rate_per_period.to_i.to_f * @principal.to_i.to_f
   
   #This is the calculations for the numerator.
-  @denominator = 1 - (1 + @intrest_rate_per_period)** @number_of_monthly_periods
+  @denominator = 1 - (1 + @intrest_rate_per_period)** -@number_of_monthly_periods
 
   #this is the payment equation
-  @paymeny = @numerator/@denominator
+  @payment = @numerator/@denominator
 
+erb(:payment_results)
 end
+
+
 get("/random/new") do
   erb(:random)
 end
 
 get("/random/results") do
-  @min_num =  params.fetch("min").to_i
-  @max_num = params.fetch("max").to_i
-  @random_num = rand(@min_num..@max_num)
+  @min =  params.fetch("min").to_i
+  @max = params.fetch("max").to_i
+  @random_num = rand(@min..@max)
   erb(:random_results)
 end
